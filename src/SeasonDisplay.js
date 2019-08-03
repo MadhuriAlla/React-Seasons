d27409c9e86e939a8ce0c81d93f85b8144c39390
MadhuriAlla <madhuribhimereddy@gmail.com>
2019-08-03 22:36:51 +0530

    Specifying Default Props

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
 const SeasonConfig = {
@@ -22,10 +23,10 @@ const GetSeason =(lat, month) => {
 const SeasonDisplay = (props) => {
     const season= GetSeason(props.lat, new Date().getMonth()); 
     const {text,iconName} = SeasonConfig[season];
-    return <div>
-        <i className={`${iconName} icon`}/>
+    return <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`}/>
         <h1>{text}</h1>
-        <i className={`${iconName} icon`}/>
+        <i className={`icon-right massive ${iconName} icon`}/>
     </div>
 }
 
